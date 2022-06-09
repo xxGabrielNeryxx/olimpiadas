b9c1bca4c6f599b2880d8a2e3035f787510830a1
Gabriel Nery <101832203+xxGabrielNeryxx@users.noreply.github.com>
2022-06-09 10:21:02 -0300

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -11,7 +11,7 @@ int main(){
     int i,j,aux;
     for(i=0;i<n;i++){
         aux=v[i];
-        for(j=i; j>=0 && v[j-1]>aux;j--) v[j]=v[j-1];
+        for(j=i; j>0 && v[j-1]>aux;j--) v[j]=v[j-1];
         v[j]=aux;
     }
     
